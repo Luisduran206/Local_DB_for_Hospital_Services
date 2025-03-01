779836c67373f68277c75df426e7de65f11e1222
Luis Durán Flores <112398598+Luisduran206@users.noreply.github.com>
2025-03-01 17:47:40 +0100

    Create Main.java
    Main class of the database. Runs the entire program.

---
+++ Main.java
@@ -0,0 +1,10 @@
+import javax.swing.SwingUtilities;
+import java.io.File;
+
+public class Main{
+
+	public static void main(String[] args) {
+		TestConexion conector = new TestConexion();
+        new Inicio(conector);
+    }
+}
